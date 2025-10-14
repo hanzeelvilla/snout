f3b4f64f9d16e038c1e845d977e829d48f4e0dbd
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-10-13 21:37:06 -0600

    Testing GPS

--- examples/gps/getLocation/getLocation.ino
+++ examples/gps/getLocation/getLocation.ino
@@ -1,39 +1,47 @@
-#define RX2 16
-#define TX2 17
-
 #include <TinyGPSPlus.h>
-
+// The TinyGPSPlus object
 TinyGPSPlus gps;
-HardwareSerial gpsSerial(2);
-
-unsigned long previousMillis = 0;
-const long interval = 1000;
-
 void setup() {
-  Serial.begin(115200);
-
-  gpsSerial.begin(115200, SERIAL_8N1, RX2, TX2);
-  Serial.println("GPS initialized");
+  Serial.begin(9600);
+  Serial2.begin(9600);
+  delay(3000);
 }
-
 void loop() {
-  while(gpsSerial.available() > 0)
-    gps.encode(gpsSerial.read());
-
-  unsigned long currentMillis = millis();
-
-  // Read data each "interval"
-  if(currentMillis - previousMillis >= interval) {
-    previousMillis = currentMillis;
-
-    if(gps.location.isValid()) {
-      Serial.print("LAT: ");
-      Serial.println(gps.location.lat(), 6);
-
-      Serial.print("LNG: ");
-      Serial.println(gps.location.lng(), 6);
-    }
-    else
-      Serial.println("GPS without signal");
-  }    
+  //updateSerial();
+  while (Serial2.available() > 0)
+    if (gps.encode(Serial2.read()))
+      displayInfo();
+  if (millis() > 5000 && gps.charsProcessed() < 10)
+  {
+    Serial.println(F("No GPS detected: check wiring."));
+    while (true);
+  }
+}
+void displayInfo()
+{
+  Serial.print(F("Location: "));
+  if (gps.location.isValid()){
+    Serial.print("Lat: ");
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print("Lng: ");
+    Serial.print(gps.location.lng(), 6);
+    Serial.println();
+  } 
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+}
+void updateSerial()
+{
+  delay(500);
+  while (Serial.available())
+  {
+    Serial2.write(Serial.read());//Forward what Serial received to Software Serial Port
+  }
+  while (Serial2.available())
+  {
+    Serial.write(Serial2.read());//Forward what Software Serial received to Serial Port
+  }
 }
